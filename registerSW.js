e550f7891b77ad2b1d846ad68b9b0f69d3d873be
laurentsar <laurentsar@users.noreply.github.com>
2026-08-02 21:56:54 +0000

    deploy: laurentsar/camping-la-perigourdine@736570cc6f21495fe197486d9464ac50e58a5d21

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/camping-la-perigourdine-releases/sw.js', { scope: '/camping-la-perigourdine-releases/' })})}
